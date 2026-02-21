1a441f629dcea67a1010fb866d07388876420c2f
bhanu1012 <bhanubasyan@gmail.com>
2026-02-21 11:05:50 +0530

    fixed CORS

--- backend/server/server.js
+++ backend/server/server.js
@@ -30,9 +30,9 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://full-stack-ecommerce-marketplace-4k46kq0fi.vercel.app",
+      "https://full-stack-ecommerce-marketplace.vercel.app/",
     ],
-    
+     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
