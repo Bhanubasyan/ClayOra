22e3381607e72caf641f4134e53009515370dcd3
bhanu1012 <bhanubasyan@gmail.com>
2026-02-21 18:08:10 +0530

    fixed some bugs

--- backend/server/server.js
+++ backend/server/server.js
@@ -10,8 +10,8 @@ const authRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
 const cartRoutes = require("./routes/cartRoutes");
 const orderRoutes = require("./routes/orderRoutes");
-const adminRoutes = require("./routes/adminRoute");
-const sellerRoutes = require("./routes/sellerRoute");
+const adminRoutes = require("./routes/adminRoutes");
+const sellerRoutes = require("./routes/sellerRoutes");
 dotenv.config();
 connectDB();
 
