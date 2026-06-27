93f7dc21183fbb25d43042ccd0e7c6f09c46549c
bhanu1012 <bhanubasyan@gmail.com>
2026-06-27 15:47:32 +0530

    Update images handler

--- backend/server/middleware/upload.js
+++ backend/server/middleware/upload.js
@@ -4,10 +4,19 @@ const cloudinary = require("../config/cloudinary");
 
 const storage = new CloudinaryStorage({
   cloudinary,
-  params: {
+  params: async (req, file) => ({
     folder: "craftroots",
-    allowed_formats: ["jpg", "png", "jpeg"],
-  },
+    format: "webp",
+    transformation: [
+      {
+        width: 800,
+        height: 800,
+        crop: "limit",
+        quality: "auto:good",
+        fetch_format: "auto",
+      },
+    ],
+  }),
 });
 
 const upload = multer({ storage });
