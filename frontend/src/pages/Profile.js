95ac0464579e3ab998025f7cf1159c29055224b3
bhanu1012 <bhanubasyan@gmail.com>
2026-06-28 09:53:12 +0530

    update structure

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -2,7 +2,7 @@ import { useEffect, useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import API from "../services/api";
 import "./profile.css";
-
+{/*Bhanu */}
 const defaultProfile = {
   phone: "",
   alternatePhone: "",
